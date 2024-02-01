d7e77f43001b0ef2dbb36467585be7ddd29ba7bc
Rupali Srivastava <rupalisri52@gmail.com>
2024-02-01 23:11:41 +0530

    Models and Serializers added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+# from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    USER_TYPE = (
+        ('ops', 'Operational User'),
+        ('client', 'Client User'),
+    )
+    username = models.CharField(max_length =80, unique=True)
+    email = models.EmailField(max_length=150)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, blank=True)
+    user_type = models.CharField(max_length=50, choices=USER_TYPE, default='client')
+    email_verified = models.BooleanField(default=False)
+    special_token = models.CharField(max_length=150, blank=True)
+
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        db_table = "user"
+
+
+class Files(models.Model):
+    id = models.AutoField(primary_key=True)
+    filename = models.CharField(max_length=100)
+    file = models.FileField(upload_to='files')
+    upload_date = models.DateTimeField(auto_now=True)
+    user_id_upload = models.ForeignKey(User, on_delete=models.SET_NULL,null=True, related_name='uploaded_files')
+    file_present = models.BooleanField(default=True)
+    user_id_delete = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='deleted_files')
+
+    def __str__(self):
+        return self.filename
+    
+    class Meta:
+        db_table = "files"
+
+
+class Download_History(models.Model):
+    id = models.AutoField(primary_key=True)
+    download_datetime = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    file = models.ForeignKey(Files, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.file.filename} - {self.download_datetime}"
+    
+    class Meta:
+        db_table = "download_history"
---
+++ app/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import User, Files, Download_History
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        user = self.context['request'].user
+
+        
+        if user.is_authenticated and user.user_type == 'ops':
+            data.pop('special_token', None)
+
+        return data
+
+class FilesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Files
+        fields = '__all__'
+
+class Download_HistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Download_History
+        fields = '__all__'
