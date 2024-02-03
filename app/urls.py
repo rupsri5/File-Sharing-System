7435d8bfd8092ba5da2f31f79d44f5c0b9326637
Rupali Srivastava <rupalisri52@gmail.com>
2024-02-03 23:34:34 +0530

    Added functionality for delete file and account, added postman collection

--- app/urls.py
+++ app/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('generate_download_link', views.generate_download_link, name='generate_download_link'),
     path('download/<uuid:uuid>', views.download, name="download"),
     path('list_file', views.list_file, name="list_file"),
+    path('list_myfile', views.list_myfile, name="list_myfile"),
+    path('delete_myfile', views.delete_myfile, name="delete_myfile"),
+    path('delete_my_account', views.delete_my_account, name="delete_my_account"),
 ]
